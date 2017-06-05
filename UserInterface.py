2b45ad11d8b3c03038d3e439a5b257440e3d65f6
Akshay Desai <akshaydesai@Akshays-MacBook-Air.local>
2017-06-05 01:06:23 -0400

    Added Sending Framework

--- UserInterface.py
+++ UserInterface.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 import sqlite3
+import os
 class DTUI:
+
+	def refreshMessages():
+		path = './incoming'
+		if os.listdir(path)==[]:
+			print "No new messages"
+		else:
+			
+
+
 
 	#Create Connection ton Database
 	conn = sqlite3.connect('./log/database.db')
@@ -20,12 +30,22 @@ class DTUI:
 	if(response== 'send'):
 		print 'Recipient?'
 		recipient=raw_input()
-		print 'Message?'
-		message=raw_input()
-		file = open('./outgoing/'+recipient+'.txt', 'w')
-		file.write(message)
-		file.close()
-		c.execute('CREATE TABLE IF NOT EXISTS "'+recipient+'" ("SeRe" "TEXT", "Message" "TEXT", "Timestamp" "TEXT" DEFAULT CURRENT_TIMESTAMP)')
+		c.execute('SELECT "Num" FROM "Contacts" WHERE "Name"="'+recipient+'"')		#Get phone number of contact
+		recipNum = c.fetchone()
+		if(recipNum==None):			#If recipient is not a contact, do not continue
+			print "Contact not found"
+		else:
+			print 'Message?'
+			message=raw_input()
+			file = open('./outgoing/'+recipNum[0]+'.txt', 'w')	#Put message in .txt to send
+			file.write(message)
+			file.close()
+
+			c.execute('CREATE TABLE IF NOT EXISTS "'+recipient+'" ("InOut" "TEXT", "Message" "TEXT", "TimeEntered" "TEXT" DEFAULT CURRENT_TIMESTAMP)')	#Create message log for this recipient
+			conn.commit()
+			c.execute("INSERT INTO '"+recipient+"' (InOut, Message) VALUES ('Out', '"+message+"')")	#Add message to recipient
+			conn.commit()
+
 	#Add a new Contact
 	elif(response=='new contact'):				#New Contact is done
 		print 'Name?'
@@ -43,5 +63,7 @@ class DTUI:
    			 else:
    			 	print "Contact not updated"
 	 	conn.commit()
+	 elif(response='check new messages'):
+
 
 
